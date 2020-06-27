8b3825c3ffd75dc071b360db53619074e40dafc5
GhillieSuit <dbstkddns4@naver.com>
2020-06-28 01:56:52 +0900

    release 1.0.0 - alpha

--- app/src/main/java/com/example/sosik/MainActivity.java
+++ app/src/main/java/com/example/sosik/MainActivity.java
@@ -1,37 +1,123 @@
 package com.example.sosik;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.res.ResourcesCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.ComponentName;
 import android.content.Intent;
+import android.content.ServiceConnection;
+import android.content.pm.PackageManager;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.speech.tts.TextToSpeech;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.Locale;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+import static android.speech.tts.TextToSpeech.ERROR;
 
+
+public class MainActivity extends AppCompatActivity implements MessageDialogFragment.Listener, View.OnClickListener {
+
+    private static final String FRAGMENT_MESSAGE_DIALOG = "message_dialog";
+    private static final String STATE_RESULTS = "results";
+    private static final int REQUEST_RECORD_AUDIO_PERMISSION = 1;
     private static boolean activityTop;
-    private RecyclerView recyclerView;
-    private ArrayList<String> list = new ArrayList<>();
+    private SpeechService mSpeechService;
+    private VoiceRecorder mVoiceRecorder;
+    private final VoiceRecorder.Callback mVoiceCallback = new VoiceRecorder.Callback() {
+
+        @Override
+        public void onVoiceStart() {
+            showStatus(true);
+            if (mSpeechService != null) {
+                mSpeechService.startRecognizing(mVoiceRecorder.getSampleRate());
+            }
+        }
+
+        @Override
+        public void onVoice(byte[] data, int size) {
+            if (mSpeechService != null) {
+                mSpeechService.recognize(data, size);
+            }
+        }
+
+        @Override
+        public void onVoiceEnd() {
+            showStatus(false);
+            if (mSpeechService != null) {
+                mSpeechService.finishRecognizing();
+            }
+        }
+
+    };
+
+    // Resource caches
+    private int mColorHearing;
+    private int mColorNotHearing;
+
+    // View references
+    private TextView mStatus;
+    private TextView mText;
+    private ResultAdapter mAdapter;
+    private RecyclerView mRecyclerView;
     private Button btnTTS;
     private EditText txtTTS;
+    private TextToSpeech tts;
+    private TextView txtSTT;
+
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder binder) {
+            mSpeechService = SpeechService.from(binder);
+            mSpeechService.addListener(mSpeechServiceListener);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mSpeechService = null;
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        InitializeService();
-        InitializeView();
+        InitializeService();    // 쉐이크 서비스 실행
+        InitializeTTS();        // TTS 생성
         SetListener();
 
-        /*for (int i=0; i<100; i++) {
-            list.add(String.format("TEXT %d", i));
-        }*/
+        final Resources resources = getResources();
+        final Resources.Theme theme = getTheme();
+        mColorHearing = ResourcesCompat.getColor(resources, R.color.status_hearing, theme);
+        mColorNotHearing = ResourcesCompat.getColor(resources, R.color.status_not_hearing, theme);
+
+        mText = (TextView) findViewById(R.id.txtSTT);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler1);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        final ArrayList<String> results = savedInstanceState == null ? null :
+                savedInstanceState.getStringArrayList(STATE_RESULTS);
+        mAdapter = new ResultAdapter(results);
+        mRecyclerView.setAdapter(mAdapter);
     }
 
     // 모션센서 서비스 실행
@@ -40,20 +126,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         startService(serviceIntent);
     }
 
-    public void InitializeView(){
-        // 리사이클러뷰에 LinearLayoutManager 객체 지정.
-        recyclerView = findViewById(R.id.recycler1) ;
-        LinearLayoutManager mLayoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(mLayoutManager);
-        // 리사이클러뷰에 SimpleTextAdapter 객체 지정.
-        SimpleTextAdapter adapter = new SimpleTextAdapter(list) ;
-        recyclerView.setAdapter(adapter) ;
-
-        btnTTS = (Button) findViewById(R.id.btnTTS);
-        txtTTS = (EditText) findViewById(R.id.txtTTS);
+    public void InitializeTTS(){
+        tts = new TextToSpeech(this, new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status != ERROR) {
+                    // 언어를 선택한다.
+                    tts.setLanguage(Locale.KOREAN);
+                    // 음성합성 속도 감소
+                    tts.setSpeechRate(0.9f);
+                }
+            }
+        });
     }
 
     public void SetListener(){
+        btnTTS = (Button) findViewById(R.id.btnTTS);
+        txtTTS = (EditText) findViewById(R.id.txtTTS);
         btnTTS.setOnClickListener(this);
     }
 
@@ -67,17 +156,34 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                     return;
                 }else{
                     txtTTS.setText("");
-                    list.add(String.format(text));
-                    recyclerView.getLayoutManager().onRestoreInstanceState(recyclerView.getLayoutManager().onSaveInstanceState());
-                    recyclerView.scrollToPosition(recyclerView.getLayoutManager().getItemCount()-1);
+                    tts.speak(text,TextToSpeech.QUEUE_FLUSH, null);
+                    mAdapter.addResult("2" + text);
+                    mRecyclerView.smoothScrollToPosition(mAdapter.getItemCount() - 1);
+                    //mRecyclerView.getLayoutManager().onRestoreInstanceState(mRecyclerView.getLayoutManager().onSaveInstanceState());
+                    //mRecyclerView.scrollToPosition(mRecyclerView.getLayoutManager().getItemCount()-1);
                 }
         }
     }
 
     @Override
-    public void onStart() {
+    protected void onStart() {
         super.onStart();
         activityTop = true;
+
+        // Prepare Cloud Speech API
+        bindService(new Intent(this, SpeechService.class), mServiceConnection, BIND_AUTO_CREATE);
+
+        // Start listening to voices
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                == PackageManager.PERMISSION_GRANTED) {
+            startVoiceRecorder();
+        } else if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.RECORD_AUDIO)) {
+            showPermissionMessageDialog();
+        } else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_RECORD_AUDIO_PERMISSION);
+        }
     }
 
     @Override
@@ -93,12 +199,171 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
+        // Stop listening to voice
+        stopVoiceRecorder();
+
+        // Stop Cloud Speech API
+        mSpeechService.removeListener(mSpeechServiceListener);
+        unbindService(mServiceConnection);
+        mSpeechService = null;
+
         super.onStop();
         activityTop = false;
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+        // TTS 객체가 남아있다면 실행을 중지하고 메모리에서 제거한다.
+        if(tts != null){
+            tts.stop();
+            tts.shutdown();
+            tts = null;
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        if (mAdapter != null) {
+            outState.putStringArrayList(STATE_RESULTS, mAdapter.getResults());
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_RECORD_AUDIO_PERMISSION) {
+            if (permissions.length == 1 && grantResults.length == 1
+                    && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                startVoiceRecorder();
+            } else {
+                showPermissionMessageDialog();
+            }
+        } else {
+            super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void startVoiceRecorder() {
+        if (mVoiceRecorder != null) {
+            mVoiceRecorder.stop();
+        }
+        mVoiceRecorder = new VoiceRecorder(mVoiceCallback);
+        mVoiceRecorder.start();
+    }
+
+    private void stopVoiceRecorder() {
+        if (mVoiceRecorder != null) {
+            mVoiceRecorder.stop();
+            mVoiceRecorder = null;
+        }
+    }
+
+    private void showPermissionMessageDialog() {
+        MessageDialogFragment
+                .newInstance(getString(R.string.permission_message))
+                .show(getSupportFragmentManager(), FRAGMENT_MESSAGE_DIALOG);
+    }
+
+    private void showStatus(final boolean hearingVoice) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                mText.setTextColor(hearingVoice ? mColorHearing : mColorNotHearing);
+            }
+        });
+    }
+
+    @Override
+    public void onMessageDialogDismissed() {
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},
+                REQUEST_RECORD_AUDIO_PERMISSION);
+    }
+
+    private final SpeechService.Listener mSpeechServiceListener =
+            new SpeechService.Listener() {
+                @Override
+                public void onSpeechRecognized(final String text, final boolean isFinal) {
+                    if (isFinal) {
+                        mVoiceRecorder.dismiss();
+                    }
+                    if (mText != null && !TextUtils.isEmpty(text)) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (isFinal) {
+                                    mText.setText(null);
+                                    mAdapter.addResult("1" + text);
+                                    mRecyclerView.smoothScrollToPosition(mAdapter.getItemCount() - 1);
+                                } else {
+                                    mText.setText(text);
+                                }
+                            }
+                        });
+                    }
+                }
+            };
+
+    private static class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView textView1 ;
+        TextView textView2 ;
+
+        ViewHolder(LayoutInflater inflater, ViewGroup parent) {
+            super(inflater.inflate(R.layout.recyclerview_item, parent, false));
+            textView1 = (TextView) itemView.findViewById(R.id.text1);
+            textView2 = (TextView) itemView.findViewById(R.id.text2);
+        }
+
+    }
+
+    private static class ResultAdapter extends RecyclerView.Adapter<ViewHolder> {
+
+        private final ArrayList<String> mResults = new ArrayList<>();
+
+        ResultAdapter(ArrayList<String> results) {
+            if (results != null) {
+                mResults.addAll(results);
+            }
+        }
+
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return new ViewHolder(LayoutInflater.from(parent.getContext()), parent);
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            String text = mResults.get(position);
+            if(text.startsWith("1")){
+                holder.textView1.setText(text.substring(1));
+                holder.textView1.setVisibility(View.VISIBLE);
+            }else if(text.startsWith("2")){
+                holder.textView2.setText(text.substring(1));
+                holder.textView2.setVisibility(View.VISIBLE);
+            }
+        }
+
+        @Override
+        public int getItemCount() {
+            return mResults.size();
+        }
+
+        void addResult(String result) {
+            mResults.add(result);
+            notifyItemInserted(getItemCount() - 1);
+        }
+
+        public ArrayList<String> getResults() {
+            return mResults;
+        }
+
     }
 
     public static boolean isActivityVisible() {
         return activityTop;
     }
+
 }
